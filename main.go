77625e9ed7906b59f2b18d6735593bc9ae3475fa
Dmitrii Morozov <33morozov@gmail.com>
2023-10-18 20:11:21 +0700

    feat: add logrus

--- main.go
+++ main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/sirupsen/logrus"
 	"os"
+	"path/filepath"
 )
 
 func main() {
+	cwd, _ := os.Getwd()
+	logFile := filepath.Join(cwd, os.Getenv("LOG_FILE"))
+	logger := logrus.New()
+	file, err := os.OpenFile(logFile, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0755)
+	if err != nil {
+		logger.SetOutput(os.Stdout)
+	}
+	defer file.Close()
+	logger.SetOutput(file)
+
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_API_TOKEN"))
 	if err != nil {
-		panic(err)
+		logger.Error(err)
+		logger.Exit(0)
 	}
 
 	bot.Debug = true
